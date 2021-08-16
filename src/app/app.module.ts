ac8bc250c97c901efa0e5e64002e20b8d9bb4152
cmaji <cmaji200@gmail.com>
2021-08-16 10:16:59 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModuleModule } from './app-routing-module/app-routing-module.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { CamelCasePipe } from './camel-case.pipe';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModuleModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   declarations: [AppComponent, HelloComponent, CamelCasePipe],
   bootstrap: [AppComponent]
 })
